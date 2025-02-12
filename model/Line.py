0d30292cd6e9dbbd781b284eae523f0f9ca64dbf
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-12 16:47:50 +0300

    make class Line normal

--- model/Line.py
+++ model/Line.py
@@ -1,8 +1,15 @@
 from Shape import Shape
+from DDAAlgorithm import DDAAlgorithm
+from BresenhamAlgorithm import BresenhamAlgorithm
+from WuAlgorithm import WuAlgorithm
+from Dot import Dot
 
 
 class Line(Shape):
-    def draw(self, alghoritm):
-        points = algorithm.compute_points(self.start, self.end)
-        for point in points:
-            canvas.create_rectangle(point[0] - 1, point[1] - 1, point[0] + 1, point[1] + 1, fill="black")
+    def __init__(self, start_dot, end_dot):
+        super().__init__()
+        self.start = start_dot
+        self.end = end_dot
+
+    def draw(self, algorithm):
+        self.dot_list = algorithm.compute_points(self.start, self.end)
