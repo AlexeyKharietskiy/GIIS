9df3642ac3c9be1cc4564e33abbb599ad1d7d135
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-13 00:32:08 +0300

    add shape draw window

---
+++ view/ShapeDrawWindow.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class ShapeDrawWindow:
+    def __init__(self, dot_list, debug_mode):
+        self.dot_list = dot_list
+        self.debug_mode = debug_mode
+        self.current_index = -1
+        self.fig, self.ax = plt.subplots(figsize=(6, 6))
+
+        self.setup_window()
+
+    def setup_window(self):
+        """Настройка окна и сетки."""
+        self.ax.set_title("Press space to draw the next pixel" if self.debug_mode else "All pixels displayed")
+        self.ax.set_xlim(0, 15)
+        self.ax.set_ylim(0, 15)
+        self.ax.grid(True, which='both', color='black', linestyle='--', linewidth=0.5)
+        self.ax.set_xticks(np.arange(0, 16, 1))
+        self.ax.set_yticks(np.arange(0, 16, 1))
+
+        if not self.debug_mode:
+            self.create_image(len(self.dot_list) - 1)
+        else:
+            self.fig.canvas.mpl_connect('key_press_event', lambda event: self.on_key(event))
+
+    def create_image(self, current_index):
+        """Создает и отображает изображение на основе текущего индекса."""
+        image = np.ones((15, 15))
+
+        for i in range(current_index + 1):
+            dot = self.dot_list[i]
+            if 0 <= dot.x < 15 and 0 <= dot.y < 15:
+                image[14 - dot.y, dot.x] = 1 - dot.intensity
+
+        self.ax.clear()
+        self.ax.imshow(image, cmap='gray', vmin=0, vmax=1, extent=(0, 15, 0, 15))
+        self.restore_window()
+        plt.draw()
+
+    def restore_window(self):
+        self.ax.set_title("Press space to draw the next pixel" if self.debug_mode else "All pixels displayed")
+        self.ax.set_xlim(0, 15)
+        self.ax.set_ylim(0, 15)
+        self.ax.grid(True, which='both', color='black', linestyle='--', linewidth=0.5)
+        self.ax.set_xticks(np.arange(0, 16, 1))
+        self.ax.set_yticks(np.arange(0, 16, 1))
+
+    def on_key(self, event):
+        if event.key == ' ':
+            if self.current_index < len(self.dot_list) - 1:
+                self.current_index += 1
+                self.create_image(self.current_index)
+            else:
+                print("All pixels have been drawn.")
+
+    @staticmethod
+    def show_shape():
+        plt.tight_layout()
+        plt.show()
+
+
