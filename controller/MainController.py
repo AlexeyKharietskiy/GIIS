8198d2422cc4a166e03c706de4ce139626e097da
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-13 01:53:54 +0300

    fix algorithm identification

--- controller/MainController.py
+++ controller/MainController.py
@@ -1,9 +1,18 @@
 from view.LineInputDataWindow import LineInputDataWindow
+from model.WuAlgorithm import WuAlgorithm
+from model.DDAAlgorithm import DDAAlgorithm
+from model.BresenhamAlgorithm import BresenhamAlgorithm
 import tkinter
+
 
 class MainController:
     def __init__(self):
         self.line_input_window = None
+        self.algorithm_dict = {
+            'WuAlgorithm': WuAlgorithm(),
+            'DDAAlgorithm': DDAAlgorithm(),
+            'BresenhamAlgorithm': BresenhamAlgorithm()
+        }
 
     def run_line_input_window(self, algorithm):
         if self.line_input_window is not None:
@@ -12,5 +21,5 @@ class MainController:
                     self.line_input_window.destroy()
             except tkinter.TclError:
                 pass
-        self.line_input_window = LineInputDataWindow(algorithm)
+        self.line_input_window = LineInputDataWindow(self.algorithm_dict[algorithm])
         self.line_input_window.run()
