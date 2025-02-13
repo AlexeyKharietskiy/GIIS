0eeaf41ecab062023b8519a397065197c5bfdded
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 02:22:34 +0300

    add hyperbola controller

---
+++ controller/HyperbolaController.py
@@ -0,0 +1,17 @@
+from view.ShapeDrawWindow import ShapeDrawWindow
+from model.Dot import Dot
+from model.Algorithms.Algorithm import Algorithm
+from model.Shapes.Hyperbola import Hyperbola
+
+
+class HyperbolaController:
+    def __init__(self, algorithm: Algorithm):
+        self.output_window = None
+        self.algorithm = algorithm
+        self.shape = None
+
+    def run_output_window(self, center, a, b, debug_mode):
+        self.shape = Hyperbola(Dot(center[0], center[1]), a, b)
+        self.shape.draw_dots(self.algorithm)
+        self.output_window = ShapeDrawWindow(self.shape, debug_mode)
+        self.output_window.show_shape()
