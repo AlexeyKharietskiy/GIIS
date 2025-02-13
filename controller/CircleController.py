e5ef1ea1a05ac45db9ca99a6fb6da2c45a6497df
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 01:15:48 +0300

    add shape class logic

--- controller/CircleController.py
+++ controller/CircleController.py
@@ -8,10 +8,10 @@ class CircleController:
     def __init__(self, algorithm: Algorithm):
         self.output_window = None
         self.algorithm = algorithm
-        self.line = None
+        self.shape = None
 
     def run_output_window(self, center, radius, debug_mode):
-        self.line = Circle(Dot(center[0], center[1]), radius)
-        dot_list = self.line.run_algorithm(self.algorithm)
-        self.output_window = ShapeDrawWindow(dot_list, debug_mode)
+        self.shape = Circle(Dot(center[0], center[1]), radius)
+        self.shape.draw_dots(self.algorithm)
+        self.output_window = ShapeDrawWindow(self.shape, debug_mode)
         self.output_window.show_shape()
--- controller/LineController.py
+++ controller/LineController.py
@@ -8,12 +8,12 @@ class LineController:
     def __init__(self, algorithm:Algorithm):
         self.output_window = None
         self.algorithm = algorithm
-        self.line = None
+        self.shape = None
 
     def run_output_window(self, start, end, debug_mode):
-        self.line = Line(Dot(start[0], start[1]), Dot(end[0], end[1]))
-        dot_list = self.line.run_algorithm(self.algorithm)
-        self.output_window = ShapeDrawWindow(dot_list, debug_mode)
+        self.shape = Line(Dot(start[0], start[1]), Dot(end[0], end[1]))
+        self.shape.draw_dots(self.algorithm)
+        self.output_window = ShapeDrawWindow(self.shape, debug_mode)
         self.output_window.show_shape()
 
 
