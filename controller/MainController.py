96df0e1605725fb8f0276b0642b201918fa36931
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 00:54:01 +0300

    add circle into main controller

--- controller/MainController.py
+++ controller/MainController.py
@@ -1,25 +1,36 @@
-from view.LineInputDataWindow import LineInputDataWindow
+from view.InputWindows.LineInputDataWindow import LineInputDataWindow
 from model.WuAlgorithm import WuAlgorithm
 from model.DDAAlgorithm import DDAAlgorithm
 from model.BresenhamAlgorithm import BresenhamAlgorithm
+from model.CircleBresenhamAlgorithm import CircleBresenhamAlgorithm
+from view.InputWindows.CircleInputWindow import CircleInputWindow
 import tkinter
 
 
 class MainController:
     def __init__(self):
-        self.line_input_window = None
+        self.input_data_window = None
         self.algorithm_dict = {
-            'WuAlgorithm': WuAlgorithm(),
-            'DDAAlgorithm': DDAAlgorithm(),
-            'BresenhamAlgorithm': BresenhamAlgorithm()
+            'Алгоритм Ву': WuAlgorithm(),
+            'Алгоритм ЦДА': DDAAlgorithm(),
+            'Алгоритм Брезенхема': BresenhamAlgorithm(),
+            'Окружность': CircleBresenhamAlgorithm()
         }
 
     def run_line_input_window(self, algorithm):
-        if self.line_input_window is not None:
+        self.check_window()
+        self.input_data_window = LineInputDataWindow(self.algorithm_dict[algorithm])
+        self.input_data_window.run()
+
+    def run_circle_input_window(self, algorithm):
+        self.check_window()
+        self.input_data_window = CircleInputWindow(self.algorithm_dict[algorithm])
+        self.input_data_window.run()
+
+    def check_window(self):
+        if self.input_data_window is not None:
             try:
-                if self.line_input_window.exist():
-                    self.line_input_window.destroy()
+                if self.input_data_window.exist():
+                    self.input_data_window.destroy()
             except tkinter.TclError:
                 pass
-        self.line_input_window = LineInputDataWindow(self.algorithm_dict[algorithm])
-        self.line_input_window.run()
