4f82b401af086dff4bff2418454fa143e3355265
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-13 16:10:22 +0300

    add dda algorithm checks

--- model/DDAAlgorithm.py
+++ model/DDAAlgorithm.py
@@ -4,6 +4,15 @@ from model.Dot import Dot
 class DDAAlgorithm(Algorithm):
     def compute_points(self, start: Dot, end: Dot):
         dot_list = []
+        steep = abs(end.y - start.y) > abs(end.x - start.x)
+        if steep:
+            start.x, start.y = self.swap(start.x, start.y)
+            end.x, end.y = self.swap(end.x, end.y)
+
+        if start.x > end.x:
+            start.x, end.x = self.swap(start.x, end.x)
+            start.y, end.y = self.swap(start.y, end.y)
+
         length = max(abs(start.x - end.x), abs(start.y - end.y))
         if length == 0:
             return[start]
@@ -11,11 +20,11 @@ class DDAAlgorithm(Algorithm):
         dy = (end.y - start.y)/ length
         x = start.x + 0.5 * self.sign(dx)
         y = start.y + 0.5 * self.sign(dy)
-        dot_list.append(Dot(int(x),int(y)))
+        dot_list.append(self.check_dot(steep, Dot(int(x),int(y))))
         for i in range(length):
             x = x + dx
             y = y + dy
-            dot_list.append(Dot(int(x),int(y)))
+            dot_list.append(self.check_dot(steep, Dot(int(x),int(y))))
 
         return dot_list
 
@@ -23,4 +32,15 @@ class DDAAlgorithm(Algorithm):
     @staticmethod
     def sign(x):
         return -1 if x < 0 else (1 if x > 0 else 0)
+
+
+    @staticmethod
+    def swap(a, b):
+        return b, a
+
+    @staticmethod
+    def check_dot(steep, dot: Dot):
+        if steep:
+            dot.x, dot.y = dot.y, dot.x
+        return dot
 
