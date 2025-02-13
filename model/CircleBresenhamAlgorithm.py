6dafd074545715675cfdc327e1e7f2360ad3b411
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 00:51:48 +0300

    add circle drawing algorithm

--- model/CircleBresenhamAlgorithm.py
+++ model/CircleBresenhamAlgorithm.py
@@ -4,15 +4,29 @@ from model.Dot import Dot
 class CircleBresenhamAlgorithm(Algorithm):
     def compute_points(self, center: Dot, radius):
         dot_list = []
+        disp_x = center.x
+        disp_y = center.y
         x = 0
         y = radius
-        d = 3 - 2 * radius
-        dot_list.append(Dot(x + center.x, y + center.y))
-        while y >= x:
-            x += 1
-            if d > 0:
+        delta = (1 - 2 * radius)
+        error = 0
+        while y >= 0:
+            dot_list.append(Dot(disp_x + x, disp_y + y))
+            dot_list.append(Dot(disp_x + x, disp_y - y))
+            dot_list.append(Dot(disp_x - x, disp_y + y))
+            dot_list.append(Dot(disp_x - x, disp_y - y))
+
+            error = 2 * (delta + y) - 1
+            if delta < 0 and error <= 0:
+                x += 1
+                delta = delta + (2 * x + 1)
+                continue
+            error = 2 * (delta - x) - 1
+            if delta > 0 and error > 0:
                 y -= 1
-                d = d + 4 * (x - y) + 10
-            else:
-                d = d + 4 * x + 6
-            dot_list.append(Dot(x + center.x, y + center.y))
+                delta = delta + (1 - 2 * y)
+                continue
+            x += 1
+            delta = delta + (2 * (x - y))
+            y -= 1
+        return dot_list
