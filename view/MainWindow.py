5757592a19cfc5e0ae50f07792d5e244c4e173e3
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 00:54:13 +0300

    add circle into main window

--- view/MainWindow.py
+++ view/MainWindow.py
@@ -7,6 +7,7 @@ class MainWindow:
     def __init__(self):
         self.controller = MainController()
         self.root = tk.Tk()
+        self.algorithm = tk.StringVar()
         self.root.resizable(False,False)
         self.root.title("Графический редактор")
         self.root.geometry("800x600")
@@ -14,11 +15,19 @@ class MainWindow:
         self.root.config(menu=self.menu_bar)
 
         # Меню для отрезков
-        self.segments_menu = Menu(self.menu_bar, tearoff=0)
-        self.segments_menu.add_command(label="Алгоритм ЦДА", command=self.draw_with_dda)
-        self.segments_menu.add_command(label="Алгоритм Брезенхема", command=self.draw_with_bresenham)
-        self.segments_menu.add_command(label="Алгоритм Ву", command=self.draw_with_wu)
-        self.menu_bar.add_cascade(label="Отрезки", menu=self.segments_menu)
+        self.line_menu = Menu(self.menu_bar, tearoff=0)
+        self.line_menu.add_radiobutton(label="Алгоритм ЦДА", variable=self.algorithm, command=self.input_line_data)
+        self.line_menu.add_radiobutton(label="Алгоритм Брезенхема", variable=self.algorithm, command=self.input_line_data)
+        self.line_menu.add_radiobutton(label="Алгоритм Ву", variable=self.algorithm, command=self.input_line_data)
+        self.menu_bar.add_cascade(label="Отрезки", menu=self.line_menu, command=self.pr_men)
+
+        # Меню для линий второго порядка
+        self.second_order_line_menu = Menu(self.menu_bar, tearoff=0)
+        self.second_order_line_menu.add_radiobutton(label="Окружность",variable=self.algorithm, command=self.input_circle_data)
+        self.second_order_line_menu.add_radiobutton(label="Эллипс", variable=self.algorithm,command=self.input_line_data)
+        self.second_order_line_menu.add_radiobutton(label="Гипербола", variable=self.algorithm,command=self.input_line_data)
+        self.second_order_line_menu.add_radiobutton(label="Парабола", variable=self.algorithm ,command=self.input_line_data)
+        self.menu_bar.add_cascade(label="Линии второго порядка", menu=self.second_order_line_menu)
 
         # Холст для рисования
         self.canvas = tk.Canvas(self.root, bg="white")
@@ -40,11 +49,20 @@ class MainWindow:
         self.root.after_idle(self.root.attributes, '-topmost', False)  # Сбрасывает после отображения
         self.root.mainloop()
 
-    def draw_with_dda(self):
-        self.controller.run_line_input_window('DDAAlgorithm')
+    def input_line_data(self):
+        self.controller.run_line_input_window(self.algorithm.get())
 
-    def draw_with_bresenham(self):
-        self.controller.run_line_input_window('BresenhamAlgorithm')
+    def input_circle_data(self):
+        self.controller.run_circle_input_window(self.algorithm.get())
 
-    def draw_with_wu(self):
-        self.controller.run_line_input_window('WuAlgorithm')
+    def input_ellipse_data(self):
+        self.controller.run_line_input_window(self.algorithm.get())
+
+    def input_hyperbola_data(self):
+        self.controller.run_line_input_window(self.algorithm.get())
+
+    def input_parabola_data(self):
+        self.controller.run_line_input_window(self.algorithm.get())
+
+    def pr_men(self):
+        print('aa')
