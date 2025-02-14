35b348b32aa092071c1c3f970043df1366294b75
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 13:43:42 +0300

    fix parabola algorithm

--- model/Algorithms/ParabolaBresenhamAlgorithm.py
+++ model/Algorithms/ParabolaBresenhamAlgorithm.py
@@ -1,31 +1,35 @@
 from model.Algorithms.Algorithm import Algorithm
 from model.Dot import Dot
 
+
 class ParabolaBresenhamAlgorithm(Algorithm):
-    def compute_points(self, vertex: Dot, a: int):
+    def compute_points(self, vertex: Dot, h: int):
         dot_list = []
-        x = 0
-        y = 0
+        x = vertex.x
+        y = vertex.y
         error = 0
+        h += y
 
         # Построение точек в первом квадранте
-        while x <= vertex.x + 100:  # Ограничиваем область построения
+        while y < h:  # Ограничиваем область построения
             dot_list.extend(self.get_parabola_points(vertex, Dot(x, y)))
+            f1 = (error <= 0 or 2 * (error - x) - 1 <= 0)
+            f2 = (error >= 0 or 2 * error + 1 > 0)
+            if f1:
+                x += 1
+                error += 2 * x + 1
 
-            # Обновляем ошибку и координаты
-            if error >= 0:
+            if f2:
                 y += 1
-                error -= 4 * a
-            x += 1
-            error += 2 * y + 1
+                error -= 1
 
         return dot_list
 
     @staticmethod
-    def get_parabola_points(vertex: Dot, dot: Dot):
+    def get_parabola_points(vertex, dot: Dot):
         # Получаем две симметричные точки (для горизонтальной параболы)
         points = [
-            Dot(vertex.x + dot.x, vertex.y + dot.y),  # Верхняя ветвь
-            Dot(vertex.x + dot.x, vertex.y - dot.y),  # Нижняя ветвь
+            dot,  # Верхняя ветвь
+            Dot(2 * vertex.x - dot.x, dot.y)  # Нижняя ветвь
         ]
         return points
