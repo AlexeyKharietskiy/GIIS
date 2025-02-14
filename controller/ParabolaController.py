97cd5630a45fab96a96280837054a16897c9cb1c
Alexey Kharietskiy <slashertoor@gmail.com>
2025-02-14 11:09:15 +0300

    add parabola controller

---
+++ controller/ParabolaController.py
@@ -0,0 +1,19 @@
+from view.ShapeDrawWindow import ShapeDrawWindow
+from model.Dot import Dot
+from model.Shapes.Parabola import Parabola
+
+
+class LineController:
+    def __init__(self, algorithm):
+        self.output_window = None
+        self.algorithm = algorithm
+        self.shape = None
+
+    def run_output_window(self, start, end, debug_mode):
+        self.shape = Parabola(Dot(start[0], start[1]), Dot(end[0], end[1]))
+        self.shape.draw_dots(self.algorithm)
+        self.output_window = ShapeDrawWindow(self.shape, debug_mode)
+        self.output_window.show_shape()
+
+
+
